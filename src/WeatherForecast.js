80c9aaa02643ee7875ed800127cbb77b976fc91f
thaihanguyen98 <142948159+thaihanguyen98@users.noreply.github.com>
2023-11-12 17:26:47 +1100

    Update WeatherIcon size

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,7 +22,7 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index < 8) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
